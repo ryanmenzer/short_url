04228daa29297456c6646f456b4683a4bc370510
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 10:02:36 -0500

    created/migrated db
    shotguned the server, updated index view

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,3 @@
 class Url < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
 end
--- db/migrate/20130918094327_create_urls.rb
+++ db/migrate/20130918094327_create_urls.rb
@@ -1,4 +1,11 @@
 class CreateUrls < ActiveRecord::Migration
   def change
+    create_table :urls do |t|
+      t.integer :user_id
+      t.string :long_url
+      t.string :short_url
+      t.integer :counter
+      t.timestamps
+    end
   end
 end
