229f587b4150bf8b5d5d24fab5696221260f56e4
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 10:22:35 -0500

    created urls view and updated index view
    working on controllers - first stage

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,16 @@
+########## GETS ##################
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
 
+get '/:short_url' do
+  # redirect to appropriate long url
+end
+
+########## POSTS ##################
+
+post '/urls' do
+  # create new short url
+end
